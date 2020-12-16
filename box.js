5947df3f04f8dbae8f97bcf13c8a6e62ab7c44d5
Raag611 <73942393+Raag611@users.noreply.github.com>
2020-12-16 16:43:39 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class box{
+ constructor(x,y,width,height) {
+    
+ var body_options = {
+  restitution : 0.7
+
+ }
+
+ this.body=Bodies.rectangle(x,y,width,height, body_options);
+ World.add(world,this.body);
+
+ this.width = width;
+ this.height = height;
+
+
+ }
+display(){
+var pos = this.body.position
+var angle = this.body.angle;
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+rectMode(CENTER)
+rect(0,0,this.width,this.height);
+pop()
+
+}
+}
